a9172d89830363aea192ace6876b651e9c50ce35
Ibrohimnarzikulov <ibrohimnarzikulov81@gmail.com>
2026-07-15 14:29:11 +0500

    w

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,36 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+  # /api/tests/, /api/scores/, /api/leaderboard/
+
+# Media files (dev only)
+
+
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+   path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
 
     # API endpoints
@@ -14,9 +43,8 @@ urlpatterns = [
     path('api/payments/', include('apps.payments.urls')),
     path('api/chat/', include('apps.chat.urls')),
     path('api/school/', include('apps.school.urls')),
-    path('api/', include('apps.exams.urls')),  # /api/tests/, /api/scores/, /api/leaderboard/
+    path('api/', include('apps.exams.urls')),
 ]
 
-# Media files (dev only)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
